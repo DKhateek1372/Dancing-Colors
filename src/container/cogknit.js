0bbed716febfc8334c0ab4a13c8e2e02560f3ad8
Deepak Dk <deepak.khateek@applaunch.io>
2020-10-03 16:55:25 +0530

    dancing color initial commit

--- src/container/cogknit.js
+++ src/container/cogknit.js
@@ -7,6 +7,19 @@ import Searchbar from '../components/UiSearch/index';
 import utils from '../utils/index';
 import '../styles/index.css';
 import 'antd/dist/antd.css'
+import { values } from 'lodash';
+
+
+const nested  = {
+  a: {
+      1:  2,
+      b:  'c',
+      c: [1, 2, 3]
+  },
+  b: [1, 3, {a: 2, b: 4, c: 5}, [5,6]],
+}
+
+const eq = [1,2,4,5,2];
 
 const colorCombo = (props) => {
 
@@ -18,23 +31,72 @@ const colorCombo = (props) => {
     setSearchState(e.target.value);
   }
 
-  const generateColor = () => {
-    let colorList = [];
-    for (var i = 0; i < search; i++) {
-      var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-      colorList.push(randomColor);
-      setColorBucket(colorList);
-    }
-  };
-
+  // const generateColor = () => {
+  //   let colorList = [];
+  //   for (var i = 0; i < search; i++) {
+  //     var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+  //     colorList.push(randomColor);
+  //     setColorBucket(colorList);
+  //   }
+  // };
   const selectedColor = (color) =>{
     setColor(color);
   }
 
-  const clearAll = () =>{
-    setSearchState(null);
-    setColorBucket([]);
+
+
+
+
+  
+
+  const nested  = {
+    a: {
+        1:  2,
+        b:  'c',
+        c: [1, 2, 3]
+    },
+    b: [1, 3, {a: 2, b: 4, c: 5}, [5,6]],
   }
+
+
+
+const generateColor = ( e )=>{
+  let arr = []
+  Object.keys(nested).map((key, index)=>{
+    Object.values(nested).map((value, i)=>{
+       Object.values(value).map((yu, i)=>{
+          if(Array.isArray(yu)){
+             
+             const myValue1 = key + '_' +  value + '_' + yu[i];
+             arr.push(myValue1)
+          }
+          else if(typeof(yu) === 'object') {
+            Object.keys(yu).map((keys,ki) =>{
+              Object.values(yu).map((value,ki) =>{
+                const myValue2 = key + '_' +  value + '_' + keys + '_' + value;
+                arr.push(myValue2)
+             })
+            })
+          }
+          else if(typeof(yu) === 'string'){
+            const myValue3 =  key + '_' +  value + '_' + yu;
+            arr.push(myValue3);
+          }
+
+        })
+      }) 
+  })
+}
+
+const clearAll = (e) =>{
+  console.log("@08734534534534",eq, (eq.length+1)/2);
+  const mid = (eq.length+1)/2;
+  for (let i = 0; i < eq.length; i++){
+     let index = eq.indexOf(mid);
+    }
+}
+
+
 
   return (
     <Container fluid>
